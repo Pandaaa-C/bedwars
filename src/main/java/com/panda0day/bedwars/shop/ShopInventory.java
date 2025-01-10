8870ca958f34aaa13b6e1ed847a8def7e68f1793
Pandaaa-C <pandacodeyt@gmail.com>
2025-01-10 13:40:13 +0000

    change itemManager creation for shop inventory

--- src/main/java/com/panda0day/bedwars/shop/ShopInventory.java
+++ src/main/java/com/panda0day/bedwars/shop/ShopInventory.java
@@ -3,7 +3,9 @@ package com.panda0day.bedwars.shop;
 import com.panda0day.bedwars.Main;
 import com.panda0day.bedwars.teams.Team;
 import com.panda0day.bedwars.teams.TeamSelector;
+import com.panda0day.bedwars.utils.ItemManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -15,13 +17,11 @@ public class ShopInventory {
         if (team == null) return;
 
         Inventory inventory = Bukkit.createInventory(null, 9 * 2, "Shop");
-        ItemStack item = new ItemStack(TeamSelector.getTeamWool(team.getIdentifier()));
-        ItemMeta meta = item.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName("Blocks");
-        item.setItemMeta(meta);
-
-        inventory.addItem(item);
+        inventory.addItem(
+                new ItemManager(TeamSelector.getTeamWool(team.getIdentifier()))
+                        .setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Blocks")
+                        .create()
+        );
 
         player.openInventory(inventory);
     }
