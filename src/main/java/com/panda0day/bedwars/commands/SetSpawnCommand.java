390af702d5cb268f3beec42755e30952108872b0
Pandaaa-C <pandacodeyt@gmail.com>
2025-01-10 15:37:14 +0000

    add setspawn location

---
+++ src/main/java/com/panda0day/bedwars/commands/SetSpawnCommand.java
@@ -0,0 +1,34 @@
+package com.panda0day.bedwars.commands;
+
+import com.panda0day.bedwars.Main;
+import com.panda0day.bedwars.utils.LocationManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SetSpawnCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(commandSender instanceof Player player)) {
+            System.out.println(commandSender.getName() + " is not a player");
+            return false;
+        }
+
+        if (!player.isOp()) {
+            player.sendMessage(Main.getMainConfig().getPrefix() + "§cYou dont have permission to use this command!");
+            return false;
+        }
+
+        if (args.length != 0) {
+            player.sendMessage(Main.getMainConfig().getPrefix() + "§6Usage: /setspawn");
+            return false;
+        }
+
+        LocationManager.setLocation("spawn", player.getLocation());
+        player.sendMessage(Main.getMainConfig().getPrefix() + "§aYou have set the spawn location ");
+
+         return true;
+    }
+}
